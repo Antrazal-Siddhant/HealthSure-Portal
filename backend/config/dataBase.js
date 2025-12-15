d11288eedd132ad2d9c2665dd15f6966103cfc20
Siddhant Mahato <siddhant.kumar@antrazal.com>
2025-12-16 00:14:44 +0530

    Corrected dB Connection

--- backend/config/dataBase.js
+++ backend/config/dataBase.js
@@ -1,24 +1,3 @@
-// const mysql = require('mysql2');
-
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root123',
-//     database: 'healthsure'
-// });
-
-// db.connect(err => {
-//     if (err) {
-//         console.error('MySQL connection failed:', err.message);
-//         return;
-//     }
-//     console.log('MySQL connected successfully');
-// });
-
-// module.exports = db;
-
-
-
 require('dotenv').config();
 const mysql = require('mysql2');
 
